ce26d292ee695b0ba8c0f9a13b40aeb0383eca7e
swethaarunachalam <swethaarunachalam21@gmail.com>
2025-02-20 21:18:06 +0530

    Add files via upload

---
+++ 2/script.js
@@ -0,0 +1,12 @@
+function celtofar() {
+    let celsius = document.getElementById("celsius").value;
+    let fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit.toFixed(2);
+}
+
+function fartocel() {
+    let fahrenheit = document.getElementById("fahrenheit").value;
+    let celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("celsius").value = celsius.toFixed(2);
+}
+
